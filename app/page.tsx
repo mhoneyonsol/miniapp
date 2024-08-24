7e3695229f1348e2e81d358167f92107b1f11074
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-08-24 20:11:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
 
     style={{
-    backgroundImage: "url('https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXB6ODQ0bW85MGJmOXFjYmpoZTY1Y2Ftd2Z1czhtbGI2ZzFiejE5aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/S8MvAa57UlmQZIhI1D/giphy.webp')"
+    backgroundImage: "url('https://i.giphy.com/xTiTniuHdUjpOlNo1q.webp')"
 }}
 
 
